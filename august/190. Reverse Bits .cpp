357634c8541c32dcc9ab748749a2f409e5355e38
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-08-12 23:41:27 +0500

    problem solved

---
+++ august/190. Reverse Bits .cpp
@@ -0,0 +1,16 @@
+#include <cstdint>
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        int i = 0;
+        while(i < 32) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
+            i++;
+        }
+        return res;
+    }
+};
