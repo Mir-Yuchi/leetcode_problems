fe137e34e5570d4985145cb9b6c8284709ab8dd7
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-08-21 21:02:08 +0500

    problem_solved

---
+++ august/563. Binary Tree Tilt .cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+
+    int postOrder(TreeNode* root, int &tilt){
+        if(root == NULL)
+            return 0;
+
+        int left = postOrder(root->left, tilt);
+        int right = postOrder(root->right, tilt);
+
+        tilt += abs(left - right);
+
+        return left + right + root->val;
+    }
+
+    int findTilt(TreeNode* root) {
+
+            int tilt = 0;
+            postOrder(root, tilt);
+            return tilt;
+    }
+};
