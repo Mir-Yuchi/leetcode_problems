b9a766ac2108418c1ccb2987ed85f736d49035ad
Mir-Yuchi <mirazimov.kr@gmail.com>
2023-08-28 20:44:22 +0500

    changes

---
+++ august/228. Summary Ranges .cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        if (nums.size() == 0) return ans;
+        int start = nums[0], end = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == end + 1) end = nums[i];
+            else {
+                if (start == end) ans.push_back(to_string(start));
+                else ans.push_back(to_string(start) + "->" + to_string(end));
+                start = end = nums[i];
+            }
+        }
+        if (start == end) ans.push_back(to_string(start));
+        else ans.push_back(to_string(start) + "->" + to_string(end));
+        return ans;
+    }
+};
